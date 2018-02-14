43b88cc9c94532f110eb872ac1438a41d93fee85
saell <amdyn@gmx.de>
2018-02-14 22:21:15 +0100

    print categories already known

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,44 @@
 extern crate personal_activity_sampler;
 
-// // struct Activity {
-// //     starttime :time::Instant,
-// //     duration: time::Duration,
-// //     endtime :time::Instant,
-// //     description :String,
-// //     categories :Box<String>
-// // }
-//
 use personal_activity_sampler::configuration;
+// struct Activity {
+//     starttime :time::Instant,
+//     duration: time::Duration,
+//     endtime :time::Instant,
+//     description :String,
+//     categories :Box<String>
+// }
 
-pub fn ask_for_activity (elapsed_time_s: u32){
+// pub fn anagrams_for<'a>( inp: &[&'a str]) -> Vec<&'a str> {
+pub fn list_categories<'a>( inp: &[&'a str]){
+    for categorie in inp {
+        println!( "{}", categorie );
+    }
+}
+
+pub fn ask_for_activity <'a>(elapsed_time_s: u32, categories: &[&'a str]){
     let elapsed_time_minutes = elapsed_time_s/60;
     println!( "What have you done the in the last {} minutes? Choose!:", elapsed_time_minutes );
 
+    list_categories( &categories );
 }
 
 pub fn run(config: configuration::Config){
 
-    ask_for_activity(config.interval_s);
+    //todo  load already known categories from file
+    let mut categories: Vec<&str> = vec!["Bugfixing", "organisatorisch", "newFeatures"];
 
-//     // let mut categories :Box<String>;
-//     //todo  fill categories from already known ones
-//     // for categorie in categories {
-//     //     println!( "{}", categorie );
-//     // }
-//     println!( "x - enter new Activity" );
-//
-//     // let mut enterNewCategorie = false;
-//
-//     //check intervall is over
-//     //ask for activity
-//
+    //Choose categories from list or enter new one
+    ask_for_activity(config.interval_s, &categories);
+
+    println!( "x - enter new Activity" );
+
+
+    // let mut enterNewCategorie = false;
+
+    //check intervall is over
+    //ask for activity
+
 }
 
 use std::{thread, time};
